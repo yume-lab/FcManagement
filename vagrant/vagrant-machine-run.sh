4f96d5d9c0a900dde0178f1d31658fe25eadc952
ippei-fukamatsu <kinakomochi1614@gmail.com>
2015-10-02 00:45:53 +0900

    マウント時の権限修正、tmp配下のdebug_kit削除

--- vagrant/vagrant-machine-run.sh
+++ vagrant/vagrant-machine-run.sh
@@ -4,6 +4,8 @@ echo "=========================================="
 echo "Set tasks to run"
 echo "=========================================="
 # apache の起動
+sudo service mysqld start
 
 # MySQLの起動
+sudo service httpd start
 
