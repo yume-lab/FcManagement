2d45544b5118297969b68eba77acaf90696f19d4
ippei-fukamatsu <kinaomochi1614@gmail.com>
2015-10-02 14:31:56 +0900

    composerインストールを手動に. あと.ideaを除外

--- vagrant/vagrant-machine-run.sh
+++ vagrant/vagrant-machine-run.sh
@@ -12,11 +12,6 @@ sudo service mysqld start
 # MySQLの起動
 sudo service httpd start
 
-# Cakeのインストール
-cd /dev_root/webapps
-composer install
-composer update
-
 # DBの作成
 mysql -u root --execute  "create database if not exists $DB_NAME"
 
