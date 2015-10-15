801caef3270e4c527adaf41638b054df9d29af45
ippei-fukamatsu <kinaomochi1614@gmail.com>
2015-10-15 18:39:59 +0900

    シフト表データのテーブル作成と、ユニーク制約に名称を追加

--- webapps/config/schema/create_table.sql
+++ webapps/config/schema/create_table.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
   created datetime NOT NULL,
   updated datetime NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (email)
+  UNIQUE uni_users(email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='ユーザーマスタ' AUTO_INCREMENT=1;
 
 -- 店舗マスタ
@@ -66,7 +66,7 @@ CREATE TABLE user_stores (
   created datetime NOT NULL,
   updated datetime NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (user_id, store_id, role_id)
+  UNIQUE uni_user_stores(user_id, store_id, role_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='ユーザー管理店舗マスタ' AUTO_INCREMENT=1;
 
 -- 従業員マスタ
@@ -88,3 +88,16 @@ CREATE TABLE employees (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='従業員マスタ' AUTO_INCREMENT=1;
 
+-- シフト管理用データ
+CREATE TABLE shift_tables (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  store_id INT unsigned NOT NULL,
+  year INT(4) unsigned NOT NULL,
+  month INT(2) unsigned NOT NULL,
+  body TEXT DEFAULT NULL ,
+  is_deleted TINYINT(1) NOT NULL DEFAULT 0,
+  created datetime NOT NULL,
+  updated datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE uni_shift_tables(store_id, year, month)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='シフト表データ' AUTO_INCREMENT=1;
