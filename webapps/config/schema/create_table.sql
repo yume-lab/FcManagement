06567b4e99c03d3f33cb6e179c373a95675c3b2a
ippei-fukamatsu <kinakomochi1614@gmail.com>
2015-11-10 18:08:44 +0900

    タイムカード用のデータ作成

--- webapps/config/schema/create_table.sql
+++ webapps/config/schema/create_table.sql
@@ -41,7 +41,7 @@ CREATE TABLE store_categories (
   created datetime NOT NULL,
   updated datetime NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (alias)
+  UNIQUE uni_store_categories(alias)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='店舗種別マスタ' AUTO_INCREMENT=1;
 
 -- ユーザーの役割マスタ
@@ -53,7 +53,7 @@ CREATE TABLE roles (
   created datetime NOT NULL,
   updated datetime NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (alias)
+  UNIQUE uni_roles(alias)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='役割マスタ' AUTO_INCREMENT=1;
 
 -- ユーザーが管理している店舗の紐付けマスタ
@@ -112,7 +112,43 @@ CREATE TABLE fixed_shift_tables (
   created datetime NOT NULL,
   updated datetime NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE uni_shift_tables(store_id, target_ym, hash)
+  UNIQUE uni_fixed_shift_tables(store_id, target_ym, hash)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='確定シフト表データ' AUTO_INCREMENT=1;
 
+-- 勤務表データ
+CREATE TABLE time_cards (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  store_id INT unsigned NOT NULL,
+  employee_id INT unsigned NOT NULL,
+  target_ym INT(6) unsigned NOT NULL,
+  body TEXT DEFAULT NULL ,
+  is_deleted TINYINT(1) NOT NULL DEFAULT 0,
+  created datetime NOT NULL,
+  updated datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE uni_time_cards(store_id, employee_id, target_ym)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='勤務表データ' AUTO_INCREMENT=1;
 
+-- 最新の勤怠データ
+CREATE TABLE latest_time_cards (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  store_id INT unsigned NOT NULL,
+  employee_id INT unsigned NOT NULL,
+  time_card_state_id INT unsigned NOT NULL,
+  created datetime NOT NULL,
+  updated datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE uni_latest_time_cards(store_id, employee_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='勤務表打刻最新データ' AUTO_INCREMENT=1;
+
+-- 勤怠状態マスタ
+CREATE TABLE time_card_states (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  alias VARCHAR(20) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  is_deleted TINYINT(1) NOT NULL DEFAULT 0,
+  created datetime NOT NULL,
+  updated datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE uni_time_card_states(alias)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='勤怠状態マスタ' AUTO_INCREMENT=1;
--- webapps/config/schema/init_data.sql
+++ webapps/config/schema/init_data.sql
@@ -15,3 +15,11 @@ VALUES
   ('/manager', '店長', '0', now(), now()),
   ('/part', 'アルバイト・パート', '0', now(), now()),
   ('/help', '他店のヘルプ', '0', now(), now());
+
+INSERT INTO time_card_states
+  (`alias`, `name`, `is_deleted`, `created`, `updated`)
+VALUES
+  ('/in', '出勤', '0', now(), now()),
+  ('/out', '退勤', '0', now(), now()),
+  ('/break_in', '休憩入', '0', now(), now()),
+  ('/break_out', '休憩終', '0', now(), now());
