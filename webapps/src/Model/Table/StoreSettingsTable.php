722c0eafc7ff7dd93e19d802a3fab6c639e2c751
ippei-fukamatsu <kinakomochi1614@gmail.com>
2015-12-03 13:03:27 +0900

    モデル作成

---
+++ webapps/src/Model/Table/StoreSettingsTable.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\StoreSetting;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * StoreSettings Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Stores
+ */
+class StoreSettingsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('store_settings');
+        $this->displayField('id');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Stores', [
+            'foreignKey' => 'store_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->add('training_minutes', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('training_minutes');
+
+        $validator
+            ->add('default_fare', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('default_fare');
+
+        $validator
+            ->allowEmpty('rested_times');
+
+        $validator
+            ->add('is_deleted', 'valid', ['rule' => 'boolean'])
+            ->requirePresence('is_deleted', 'create')
+            ->notEmpty('is_deleted');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['store_id'], 'Stores'));
+        return $rules;
+    }
+}
